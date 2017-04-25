a63db4cc2280d6017d2af2997141e96ce37c7218
ehennenfent <ehennenfent@imsa.edu>
2017-04-25 03:36:58 -0500

    Better in-frame rendering

--- noisecanvas.py
+++ noisecanvas.py
@@ -1,12 +1,13 @@
 from noise import snoise2
-from tkinter import Tk, mainloop, Canvas, PhotoImage
+from tkinter import *
 import numpy as np
 from scipy.spatial import Voronoi
 import math
 from colorsys import hsv_to_rgb
 from random import randint
+import progressbar
 
-canvas_width = 512
+canvas_width = 2048
 canvas_height = canvas_width
 freq = canvas_width / 2
 num_poi = int(256 * math.log(max(canvas_width, canvas_height), 2))
@@ -48,16 +49,25 @@ def get_centroid(_region):
     return _newx, _newy
 
 master = Tk()
-
-
-w = Canvas(master, width=canvas_width, height=canvas_height)
-w.pack()
+frame = Frame(master)
+frame.grid(row=0,column=0)
+frame.pack()
+w = Canvas(frame, width=canvas_width, height=canvas_height, scrollregion=(0,0,canvas_width,canvas_height))
+hbar=Scrollbar(frame,orient=HORIZONTAL)
+hbar.pack(side=BOTTOM,fill=X)
+hbar.config(command=w.xview)
+vbar=Scrollbar(frame,orient=VERTICAL)
+vbar.pack(side=RIGHT,fill=Y)
+vbar.config(command=w.yview)
+w.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+w.pack(side=LEFT,expand=True, in_=frame)
 
 img = PhotoImage(width=canvas_width, height=canvas_height)
 w.create_image((canvas_width/2, canvas_height/2), image=img, state="normal")
 
 print("Generating Simplex Noise")
-for x in range(canvas_width):
+xbar = progressbar.ProgressBar()
+for x in xbar(range(canvas_width)):
     for y in range(canvas_height):
         hScale = simplex_wrapper(x, y, height_offset)
         tScale = simplex_wrapper(x, y, temperature_offset)
@@ -94,5 +104,8 @@ for region in vor.regions:
 
 for point in vor.points:
     img.put("#%02x%02x%02x" % (255, 0, 0), (cv(point[0]), cv(point[1], dim='y')))
+
+w.update()
+w.postscript(file="render.ps", colormode='color', width=canvas_width, height=canvas_height)
 
 mainloop()
