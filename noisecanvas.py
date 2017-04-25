ebf03d1bf0dd9177ab52ef4c051c18c9c735b23c
ehennenfent <ehennenfent@imsa.edu>
2017-04-24 20:37:28 -0500

    Added partial stages

--- noisecanvas.py
+++ noisecanvas.py
@@ -1,8 +1,8 @@
 from noise import snoise2
 from tkinter import *
 
-canvas_width = 512
-canvas_height = 512
+canvas_width = 1024
+canvas_height = 1024
 freq = canvas_width / 2
 
 master = Tk()
@@ -12,6 +12,11 @@ w.pack()
 
 img = PhotoImage(width=canvas_width, height=canvas_height)
 w.create_image((canvas_width/2, canvas_height/2), image=img, state="normal")
+
+print("Generating Height Map")
+print("Generating Temperature Map")
+print("Consolidating Colors")
+print("Rendering")
 
 r, g, b = 255, 255, 255
 for x in range(canvas_width):
