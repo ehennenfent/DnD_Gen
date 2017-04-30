217dbc78dbaf8cba90d4c9e186a466d933715e65
Eric Hennenfent <ehennenfent@imsa.edu>
2017-04-30 02:01:42 -0500

    Added Icons

---
+++ dice.py
@@ -0,0 +1,22 @@
+from random import randint
+
+def d4():
+    return randint(1,4)
+
+def d6():
+    return randint(1,6)
+
+def d8():
+    return randint(1,8)
+
+def d10():
+    return randint(1,10)
+
+def d12():
+    return randint(1,12)
+
+def d20():
+    return randint(1,20)
+
+def d100():
+    return randint(1,100)
